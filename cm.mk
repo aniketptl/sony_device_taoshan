fda95762dde3cc60668e9af3292cc440298c4d93
aniketroxx <aniketp563@gmail.com>
2014-05-18 18:27:54 +0530

    Liquify

--- cm.mk
+++ cm.mk
@@ -16,14 +16,14 @@
 $(call inherit-product, device/sony/taoshan/taoshan.mk)
 
 
-# Inherit CM common GSM stuff.
+# Inherit liquid common GSM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
 
 TARGET_SCREEN_HEIGHT := 854
 TARGET_SCREEN_WIDTH := 480
@@ -32,7 +32,7 @@ TARGET_SCREEN_WIDTH := 480
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C2105_1275-0241 BUILD_FINGERPRINT=Sony/C2105_1275-0241/C2105:4.2.2/15.3.A.0.26/Android.0031:user/release-keys PRIVATE_BUILD_DESC="C2105-user 4.2.2 JDQ39 Android.0031 test-keys"
 
 PRODUCT_DEVICE := taoshan
-PRODUCT_NAME := cm_taoshan
+PRODUCT_NAME := liquid_taoshan
 PRODUCT_BRAND := Sony
 PRODUCT_MODEL := C2105
 PRODUCT_MANUFACTURER := Sony
